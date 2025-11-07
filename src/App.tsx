380102937e92bb67b467982aa14b3545fc6024d6
Amlan Mondal <amlanmondal@Amlans-MacBook-Air.local>
2025-11-08 01:04:51 +0530

    add git lab deploy action

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-white dark:bg-black p-5 font-mono '>Something exciting is on the way!!</h1>
+    </>
+  )
+}
+
+export default App
