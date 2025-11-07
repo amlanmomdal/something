c54ac74f4d480939a82b542a1333e14346cc5c63
Amlan Mondal <amlanmondal@Amlans-MacBook-Air.local>
2025-11-08 01:16:38 +0530

    git hub deploy code action

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-white dark:bg-black p-5 font-mono '>Something exciting is on the way!!</h1>
+      <h1 className='bg-white dark:bg-black p-5 font-mono '>Something exciting is on the way!</h1>
     </>
   )
 }
