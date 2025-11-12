addb4d96c1f963544033ab651ec0e1b8d2300bf4
Amlan Mondal <amlanmondal@Amlans-MacBook-Air.local>
2025-11-13 00:02:58 +0530

    lick to make it disappear

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
-import Counter from './components/Counter'
 import List from './components/List'
 
 function App() {
 
   return (
     <>
-    {/* <Counter /> */}
     <List />
     </>
   )
